7fa39780bd6c37551dffccd8f17280a60ead82d3
hezethegamer <hezethegamer01@gmail.com>
2023-10-30 13:13:24 -0700

    bnj

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert a node as the left-child of another node
+ *
+ * @parent: node parent
+ * @value: node value
+ * Return: pointer to  new node or NULL failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	new->left = parent->left;
+	new->right = NULL;
+
+	if (parent->left != NULL)
+		parent->left->parent = new;
+
+	parent->left = new;
+
+	return (new);
+}
